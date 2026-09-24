538eb48d4ad80ca5659ca9e43fe4c198c04c6481
Ariel M. Perez <arielmanuelperez1982@gmail.com>
2026-09-24 20:52:32 -0300

    Primer commit: proyecto 100 one more night listo para render

---
+++ app_menu.bat
@@ -0,0 +1,14 @@
+@echo off
+title Servidor Menu Boliche
+color 0A
+echo ========================================================
+echo       INICIANDO SERVIDOR LOCAL DE BEBIDAS...
+echo ========================================================
+echo.
+
+REM Inicia el script de Python
+python app_menu.py
+
+echo.
+echo El servidor se ha detenido.
+pause
